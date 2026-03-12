222e47a4030edf85f3036b3a5c8c91fd71dd1890
Hugo Raskin <raskin.hugo@gmail.com>
2026-03-12 15:48:46 +0100

    Avoid error when parsing no existing metric

--- src/main/java/org/snail/prequalbackend/model/Metrics.java
+++ src/main/java/org/snail/prequalbackend/model/Metrics.java
@@ -3,11 +3,11 @@ package org.snail.prequalbackend.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record Metrics(
-        @JsonProperty("code_smells") int codeSmells,
-        @JsonProperty("cognitive_complexity") int cognitiveComplexity,
-        int complexity,
-        @JsonProperty("development_cost") int developmentCost,
-        @JsonProperty("duplicated_lines") int duplicatedLines,
-        int ncloc,
-        @JsonProperty("software_quality_maintainability_rating") int softwareQualityMaintainabilityRating
+        @JsonProperty("code_smells") Integer codeSmells,
+        @JsonProperty("cognitive_complexity") Integer cognitiveComplexity,
+        Integer complexity,
+        @JsonProperty("development_cost") Integer developmentCost,
+        @JsonProperty("duplicated_lines") Integer duplicatedLines,
+        Integer ncloc,
+        @JsonProperty("software_quality_maintainability_rating") Integer softwareQualityMaintainabilityRating
 ) {}
